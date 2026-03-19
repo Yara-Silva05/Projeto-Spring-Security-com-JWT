50f2eeddd2e7172e36d9452ce2804da25afbfeb3
Yara-Silva05 <yara.silva.alves05@gmail.com>
2026-03-19 20:08:14 -0300

    Início do projeto.

---
+++ src/main/java/yara/projeto_springSecurity_JWT/ProjetoSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package yara.projeto_springSecurity_JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yara/projeto_springSecurity_JWT/ProjetoSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package yara.projeto_springSecurity_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
