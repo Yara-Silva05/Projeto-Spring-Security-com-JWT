4a4acc25d756e1297a50f01cd9763a9d8a77ba7f
Yara-Silva05 <yara.silva.alves05@gmail.com>
2026-03-19 21:50:48 -0300

    Feat: Criação dos repositórios.

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import models.PostModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PostRepository extends JpaRepository<PostModel, Long> {
+}
---
+++ src/main/java/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import models.RoleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleModel, Long> {
+}
+
